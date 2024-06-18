9287f1e74ad8296e5dc5fbe94811435dc79dd0f1
armagank <karakas.e@northeastern.edu>
2024-06-19 01:48:53 +0300

    experiences added and home page breakpoints added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,21 +1,33 @@
 const experienceData = [
   {
+    year: "July – Dec. 2024",
+    position: "IT Desk Co-op - Unifirst",
+    description:
+      "Provided technical support and troubleshooting assistance to customers, resolving hardware, software, and network issues to ensure seamless business operations.",
+  },
+  {
+    year: "Sept - Dec 2023",
+    position: "Athletic Chair - Delta Kappa Epsilon Northeastern",
+    description:
+      "Led sports and fitness initiatives, fostering teamwork and healthy lifestyles among fraternity members. Organized and managed inter-fraternity athletic competitions, enhancing community engagement and promoting physical well-being. ",
+  },
+  {
     year: "June 2023 - Aug 2023",
     position: "Inter Web Developer - KarakaslarGroup",
     description:
       "Enhanced Karakaslar Group's project management through a custom website created utilizing object-oriented PHP, HTML, CSS, and JavaScript. Streamined tasks across roles (manager, employee, accountant). Improved efficiency and accountability with role-based access, categorized entries, and robust search capabilities.",
   },
   {
-    year: "2018 - 2019",
-    position: "Web Developer - Company",
+    year: "Aug 2020",
+    position: "Intern - Cloudyflex",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti ex eos commodi quibusdam quos quas optio explicabo dolor repellendus.",
+      "Interned at a cloud technology company, learned ZOHO software for varied business processes. Contributed to the Message Management System (MMS) by assisting with data management tasks.",
   },
   {
-    year: "2019 - 2020",
-    position: "Web Developer - Company",
+    year: "July 2020",
+    position: "Intern - MobileX",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti ex eos commodi quibusdam quos quas optio explicabo dolor repellendus.",
+      "Trained in mobile applications for iOS and Android, gained back-end system experience. Participated in the Aura Dashboard project, observing database queries and analytics presentation on an interactive dashboard.",
   },
 ];
 
