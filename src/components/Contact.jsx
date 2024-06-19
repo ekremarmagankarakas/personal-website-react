d7e14ddef66524d0dc03b45027034f447c472e3a
armagank <karakas.e@northeastern.edu>
2024-06-19 23:23:55 +0300

    contact better

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,29 +6,39 @@ function Contact() {
       </h2>
       <div className="contact-main">
         <div className="contact-grid">
-          <div className="contact-social-icons">
-            <a
-              href="https://github.com/ekremarmagankarakas"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            <span class="fa fa-github"></span>
-            </a>
-            <a
-              href="https://linkedin.com/in/ekrem-armagan-karakas"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            <span class="fa fa-linkedin"></span>
-            </a>
+          <div className="contact-socials">
+            <h3>Socials</h3>
+            <p>Below are my social links. You can access my codes through github.</p>
+            <div className="contact-social-icons">
+              <a
+                href="https://github.com/ekremarmagankarakas"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              <span class="fa fa-github"></span>
+              </a>
+              <a
+                href="https://linkedin.com/in/ekrem-armagan-karakas"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              <span class="fa fa-linkedin"></span>
+              </a>
+            </div>
           </div>
           <div className="contact-phone">
-            <h1>Phone:</h1> <p>(857) 971-0526</p>
+            <h3>Phone</h3>
+            <p>You can access me through one of the following phone number, however, I may not be able to answer right away. You can reach me through whatsapp.</p>
+            <h1>US Phone:</h1> <p>+1 (857) 971-0526</p>
           </div>
           <div className="contact-email">
+            <h3>Email</h3>
+            <p>You can contact via the email below anytime you want. I will make sure to get back to you as soon as possible.</p>
             <h1>Email:</h1> <p>ekremarmagankarakas@gmail.com</p>
           </div>
           <div className="contact-address">
+            <h3>Location</h3>
+            <p>I am currently located at Boston studying at Northeastern University.</p>
             <h1>Location</h1> <p>Boston, MA</p>
           </div>
         </div>
